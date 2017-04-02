ffff01117a9a1119adbf29bfaa36afbd79004cd6
wook <rudollee@gmail.com>
2017-04-03 03:15:06 +0900

    Async

---
+++ LanguageFeatures/Models/MyAsyncMethods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures.Models
+{
+	public class MyAsyncMethods
+	{
+		public static Task<long?> GetPageLength()
+		{
+			HttpClient client = new HttpClient();
+			var httpTask = client.GetAsync("http://apress.com");
+
+			// another task while completing http request.
+
+			return httpTask.ContinueWith((Task<HttpResponseMessage> antecedent) =>
+			{
+				return antecedent.Result.Content.Headers.ContentLength;
+			});
+		}
+	}
+}
