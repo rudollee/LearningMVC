0a9ec180c113357b1f61dc32b2822ed211a4542a
wook <rudollee@gmail.com>
2017-04-06 01:39:01 +0900

    Ch 7. SportsStore Step 2

--- SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using Moq;
 using Ninject;
 using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Concrete;
 using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Infrastructure
@@ -32,15 +33,17 @@ namespace SportsStore.WebUI.Infrastructure
 
 		private void AddBindings()
 		{
-			Mock<IProductRepository> mock = new Mock<IProductRepository>();
-			mock.Setup(m => m.Products).Returns(new List<Product>
-			{
-				new Product { Name = "Football", Price = 25 },
-				new Product { Name = "Surf board", Price = 179 },
-				new Product { Name = "Running shoes", Price = 95}
-			});
+			//Mock<IProductRepository> mock = new Mock<IProductRepository>();
+			//mock.Setup(m => m.Products).Returns(new List<Product>
+			//{
+			//	new Product { Name = "Football", Price = 25 },
+			//	new Product { Name = "Surf board", Price = 179 },
+			//	new Product { Name = "Running shoes", Price = 95}
+			//});
 
-			kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+			kernel.Bind<IProductRepository>().To<EFProductRepository>();
+
+
 		}
 	}
 }
