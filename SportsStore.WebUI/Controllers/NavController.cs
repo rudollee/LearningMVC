9053410db8cd1d41743e29e574acdd9f54e6873a
wook <rudollee@gmail.com>
2017-04-08 03:51:50 +0900

    Ch. 10 SportsStore Mobile

--- SportsStore.WebUI/Controllers/NavController.cs
+++ SportsStore.WebUI/Controllers/NavController.cs
@@ -15,7 +15,7 @@ namespace SportsStore.WebUI.Controllers
 			repository = repo;
 		}
 
-		public PartialViewResult Menu(string category = null)
+		public PartialViewResult Menu(string category = null /*, bool horizontalLayout = false */)
 		{
 			ViewBag.SelectedCategory = category;
 			IEnumerable<string> categories = repository.Products
@@ -23,7 +23,9 @@ namespace SportsStore.WebUI.Controllers
 									.Distinct()
 									.OrderBy(x => x);
 
-			return PartialView(categories);
+			//string viewName = horizontalLayout ? "MenuHorizontal" : "Menu";
+
+			return PartialView("FlexMenu", categories);
 		}
 
 		//public string Menu()
