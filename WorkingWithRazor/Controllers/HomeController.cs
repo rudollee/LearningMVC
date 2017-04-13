a3ef7da060985baa0e53498f21df32f1e2f3ddc4
wook <rudollee@gmail.com>
2017-04-13 21:41:51 +0900

    Ch. 20 View - WorkingWithRazor

---
+++ WorkingWithRazor/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WorkingWithRazor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+			string[] names = { "Apple", "Orange", "Pear" };
+            return View(names);
+        }
+
+		public ActionResult List()
+		{
+			return View();
+		}
+
+		[ChildActionOnly]
+		public ActionResult Time()
+		{
+			return PartialView(DateTime.Now);
+		}
+    }
+}
