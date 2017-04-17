1eb39e5afc7bf3605a02a68f08a93f26f913bba2
wook <rudollee@gmail.com>
2017-04-18 00:50:18 +0900

    Ch. 21 Helper Methods

---
+++ HelperMethods/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HelperMethods.Models;
+
+namespace HelperMethods.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+			ViewBag.Fruits = new string[] { "Spple", "Orange", "Pear" };
+			ViewBag.Cities = new string[] { "New York", "London", "Paris" };
+
+			string message = "This is an HTML element: <input>";
+
+            return View((object)message);
+        }
+
+		public ActionResult CreatePerson()
+		{
+			return View(new Person());
+		}
+
+		[HttpPost]
+		public ActionResult CreatePerson(Person Person)
+		{
+			return View(Person);
+		}
+    }
+}
